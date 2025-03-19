237014913eaaf70a193eb9d25e3643f610c5b0bd
liamstamper <liam.stamper@gmail.com>
2025-03-19 16:55:22 -0500

    corrected github app link

--- app/quickstart/page.tsx
+++ app/quickstart/page.tsx
@@ -5,7 +5,7 @@ const testdata = {
   title: "Lamp QuickStart",
   subtitle: "",
   content: [
-    "**Set Up:** First you add the app to your repositiory. To do this navigate to [https://github.com/apps/NAME_OF_YOUR_APP](https://github.com/apps/NAME_OF_YOUR_APP)",
+    "**Set Up:** First you add the app to your repositiory. To do this navigate to [https://github.com/apps/lamp-pr-bot](https://github.com/apps/lamp-pr-bot)",
     "This guide walks you through setting up and running your AI-powered code review bot using Vite and Hono. The bot integrates with GitHub, listens for commits and pull requests, and provides automated AI-generated feedback.",
     " With AI-driven tools, developers can now automate large parts of the review process, detecting bugs, suggesting improvements, and enforcing coding standards with minimal human intervention.",
     "In this article, we explore how AI-powered code review bots, like the one we’re building, can enhance team efficiency and productivity.",
